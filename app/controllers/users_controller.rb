ea5236d8ce5e3d5f05193c83e9ade251dcc76c57
Lizzie <lizziewrab@me.com>
2022-10-25 16:03:18 -0500

    added users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def index
+    users = User.all
+    render json: users, include: :items
+  end
+
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show] do 
+  resources :users, only: [:show, :index ] do 
     resources :items, only: [:index, :show, :create]
   end
 end
